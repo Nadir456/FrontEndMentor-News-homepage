ed2162bff03a3cbff8b462cf097138bd38458724
Nadir456 <153538447+Nadir456@users.noreply.github.com>
2024-01-14 08:42:48 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function showmenu(){
+      
+    document.getElementById("mobilescreen").style.display="block";
+    document.getElementById("closebtn").style.display="block";
+    document.getElementById("menubtn").style.display="none";
+}
+
+function closemenu(){
+    document.getElementById("mobilescreen").style.display="none";
+    document.getElementById("closebtn").style.display="none";
+    document.getElementById("menubtn").style.display="block";
+}
